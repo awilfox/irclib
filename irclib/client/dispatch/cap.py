d428c84a53f4df0c90e69db166929c924c5911c0
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2013-02-23 17:08:02 -0600

    Move ACK stuff into its own function

--- irclib/client/dispatch/cap.py
+++ irclib/client/dispatch/cap.py
@@ -1,18 +1,22 @@
 """ Dispatches CAP stuff """
 def dispatch_cap(client, line):
     if line.params[1] == 'ACK':
-        # Cancel
-        client.timer_cancel('cap_terminate')
+        dispatch_cap_ack(client, line)
 
-        # Caps follow
-        client.supported_cap = line.params[-1].lower().split()
 
-        if 'tls' in client.supported_cap and client.use_starttls and not client.use_ssl:
-            # Start TLS negotiation
-            client.cmdwrite('STARTTLS')
-        else:
-            # Register only if we don't need STARTTLS
-            client.dispatch_register()
+def dispatch_cap_ack(client, line):
+    # Cancel
+    client.timer_cancel('cap_terminate')
+
+    # Caps follow
+    client.supported_cap = line.params[-1].lower().split()
+
+    if 'tls' in client.supported_cap and client.use_starttls and not client.use_ssl:
+        # Start TLS negotiation
+        client.cmdwrite('STARTTLS')
+    else:
+        # Register only if we don't need STARTTLS
+        client.dispatch_register()
 
 
 hooks_in = (
