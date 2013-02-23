df30fd2eee9313b6da3b2f2d46fbe0cf8a3d54a7
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2013-02-23 13:54:10 -0600

    Make the channel class useful

--- irclib/client/channel.py
+++ irclib/client/channel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import weakref
+from irclib.common.modes import ModeSet
 
 class Channel:
     def __init__(self, network, name):
@@ -9,7 +10,16 @@ class Channel:
 
         self.users = weakref.WeakValueDictionary()
 
-        self.modes = network.modes[self.name]
+        # Gather these from ISUPPORT
+        # Note there is a race here - if you instantiate this, and ISUPPORT is
+        # not in yet, you WILL get incorrect modes until you update this. YOU
+        # HAVE BEEN WARNED.
+        p_prefix = ''.join(p[0] for p in network.isupport['PREFIX'])
+        p_list, p_set, p_both, = network.isupport['CHANMODES'][:3]
+
+        self.modes = ModeSet(p_list=p_list, p_set=p_set, p_both=p_both,
+                             p_prefix=p_prefix)
 
         # Are we parting the channel?
         self.parting = False
+
