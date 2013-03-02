be1efc22fc91b5dbacc6ea9099e22c8c27565930
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2013-03-02 10:48:21 -0600

    Massive overhaul to support nonblocking operation.

--- irclib/client/network.py
+++ irclib/client/network.py
@@ -29,6 +29,7 @@ class IRCClientNetwork(object):
         self.port = kwargs.get('port')
         self.use_ssl = kwargs.get('use_ssl', False)
         self.use_starttls = kwargs.get('use_starttls', True)
+        self.blocking = kwargs.get('blocking', True)
 
         if any(e is None for e in (self.host, self.port)):
             raise RuntimeError('No valid host or port specified')
@@ -45,6 +46,17 @@ class IRCClientNetwork(object):
         if self.use_ssl:
             # Unneeded and probably harmful. :P
             self.use_starttls = False
+
+        # Non-blocking errors
+        errs = ('EINPROGRESS', 'EWOULDBLOCK', 'EAGAIN', 'EINTR', 'ERESTART',
+                'ENOBUFS', 'ENOENT')
+        self.nonblock = list(filter(None, [getattr(errno, e, None) for e in
+                                           errs]))
+
+        if ssl:
+            sslerrs = (ssl.SSL_ERROR_WANT_READ, ssl.SSL_ERROR_WANT_WRITE,
+                       ssl.SSL_ERROR_WANT_CONNECT)
+            self.nonblock.extend(sslerrs)
 
         # Connection flag
         self.connected = False
@@ -125,17 +137,22 @@ class IRCClientNetwork(object):
         with self.connlock:
             if not self.connected:
                 self.sock = socket.socket()
-                self.__buffer = ''
+                self.setblocking(self.blocking)
+
+                if self.use_ssl and not self.use_starttls:
+                    self.wrap_ssl()
+
+                self.send_buffer = bytes()
+                self.recv_buffer = bytes() 
                 self.ssl_wrapped = False
                 self.reset()
 
             if timeout is not None:
                 self.sock.settimeout(timeout)
-            self.sock.connect((self.host, self.port))
-            self.connected = True
 
-            if self.use_ssl and not self.use_starttls:
-                self.wrap_ssl()
+            self.sock.connect((self.host, self.port))
+            if self.blocking:
+                self.connected = True
 
 
     """ Wrap the socket in SSL """
@@ -147,44 +164,79 @@ class IRCClientNetwork(object):
             if self.ssl_wrapped:
                 self.logger.warn('Attempting to wrap SSL-wrapped class')
 
-            self.sock = ssl.wrap_socket(self.sock)
+            try:
+                self.sock = ssl.wrap_socket(self.sock)
+            except (IOError, OSError) as e:
+                if e.errno in self.nonblock:
+                    self.use_ssl = True
+                    self.ssl_wrapped = True
+                raise
+
             self.use_ssl = True
             self.ssl_wrapped = True
+
+
+    """ Set the socket non-blocking """
+    def setblocking(self, block):
+        with self.connlock:
+            self.sock.setblocking(block)
+            self.blocking = block
 
 
     """ Recieve data from the wire """
     def recv(self):
         with self.inlock:
+            # Assume connected
+            self.connected = True
+
             self.sock.settimeout(None)
-            while '\r\n' not in self.__buffer:
-                try:
-                    data = self.sock.recv(2048)
-                except socket.error:
+            try:
+                data = self.sock.recv(2048)
+            except (IOError, OSError) as e:
+                if e.errno not in self.nonblock:
                     self.connected = False
-                    raise
+                raise
 
-                if not data:
-                    socketerror(errno.ECONNRESET, instance=self)
+            if not data:
+                socketerror(errno.ECONNRESET, instance=self)
 
-                self.__buffer += data.decode('UTF-8', 'replace')
+            self.recv_buffer += data
+            lines = self.recv_buffer.split(b'\r\n')
+            self.recv_buffer = lines.pop()
 
-            lines = self.__buffer.split('\r\n')
-            self.__buffer = lines.pop() 
+            if lines:
+                lines = [l.decode('utf-8', 'replace') for l in lines]
 
             return lines
 
 
     """ Send data onto the wire """
-    def send(self, data):
+    def send(self, data=None):
         with self.outlock:
-            sendlen = len(data)
-            curlen = 0
-            while curlen < sendlen:
-                try:
-                    curlen += self.sock.send(data[curlen:])
-                except socket.error:
+            # Assume connected
+            self.connected = True
+
+            if data:
+                self.send_buffer += data
+                if not self.blocking:
+                    # Non-blocking mode
+                    return
+
+            if not self.send_buffer:
+                return
+
+            try:
+                sendlen = self.sock.send(self.send_buffer)
+                self.send_buffer = self.send_buffer[sendlen:]
+            except (IOError, OSError) as e:
+                if e.errno not in self.nonblock:
                     self.connected = False
-                    raise
+                raise
+
+            if self.blocking:
+                # Drain the buffer in blocking mode
+                while self.send_buffer:
+                    self.send()
 
 
     """ Default oneshot timer implementation """
